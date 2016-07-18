314c3eb6306b56bce84672bca344958017763083
MtGam <theresm@gmail.com>
2016-07-18 17:39:34 -0400

    added methods to user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new
+
+    if @user.save
+      redirect_to user_url(@user), notice: "Signed up!"
+    else
+      render :new, notice: "Please try again."
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to user_url(@user)
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+end
