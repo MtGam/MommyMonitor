0420f4b1301389dd54c080d70dca5d61fa6928e6
chau nguyen <nhbc111@yahoo.com>
2016-07-20 17:18:30 -0400

    debugged sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  #get 'sessions/new'
+
 
   root 'home#index'
 
@@ -20,33 +20,7 @@ Rails.application.routes.draw do
   end
 
 
-  # get 'sessions/new'
-  #
-  # get 'sessions/create'
-  #
-  # get 'sessions/destroy'
 
-
-  # get 'comments/index'
-  #
-  # get 'comments/new'
-  #
-  # get 'comments/show'
-  #
-  # get 'comments/create'
-  #
-  # get 'comments/edit'
-
-  # get 'users/index'
-  #
-  # get 'users/new'
-  #
-  # get 'users/show'
-  #
-  # get 'users/edit'
-=======
-
->>>>>>> b4680549a28b3eeeed82d667b67ee06423430e42
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
