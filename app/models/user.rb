ad2f2012d3903d5bd3dd417a862b70f72b05ef77
MtGam <theresm@gmail.com>
2016-07-23 12:28:34 -0400

    no changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,6 @@ class User < ApplicationRecord
   validates :password, length: {minimum: 7, too_short: "Password too short. Minimum 7 characters."}
 
 
-  validates_inclusion_of :tri_1, in: [true, false]
-  validates_inclusion_of :tri_2, in: [true, false]
-  validates_inclusion_of :tri_3, in: [true, false]
-
   has_secure_password
   has_many :comments
 end
