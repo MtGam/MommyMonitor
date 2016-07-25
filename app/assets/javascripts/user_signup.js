810160a484cd1d3e93c581681efeb30436e7e676
MtGam <theresm@gmail.com>
2016-07-25 14:11:51 -0400

    jquery on signup buttons revisited

--- app/assets/javascripts/user_signup.js
+++ app/assets/javascripts/user_signup.js
@@ -2,7 +2,24 @@ $(document).on('ready page:load', function(){
   $('.role1').hide();
   $('.role2').hide();
 });
+
 $(document).ready(function(){
+  $('#r1').hover(function(){
+    $(this).css('background-color', '#cc9966');  //#e6e6ff
+  }, function () {
+    $(this).css('background-color', '#e6ffe6');
+  });
+});
+
+$(document).ready(function(){
+  $('#r2').hover(function(){
+    $(this).css('background-color', '#cc9966');
+  }, function () {
+    $(this).css('background-color', '#ffffe6');
+  });
+});
+
+  $(document).ready(function(){
     $("#r1").click(function(){
       $('#user_doc_qual').val('');
       $('#user_regis_number').val('');
